66364c25305e4f5dd1ed99c9eaa660ac8496a5b6
ifa02.timmjuli <1julian.timmer1@gmail.com>
2021-04-12 11:41:48 +0200

    variables

--- Hangman/main.c
+++ Hangman/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+const maxMistakes = 10;
+
+char player1[];
+//char player2[];
+char solution[];
+char hangmanPrint[];
+
+int start; //GetTickCount(int)
+
+int tries;
+char triedLetters[];
+char correctLetters[];
+int mistakes;
+int success = 0;
 
 int main()
 {
