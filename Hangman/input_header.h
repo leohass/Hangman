90ae90125b3ce3a95d912210c4c4be52b89dc193
masouri <61315462+masouri@users.noreply.github.com>
2021-04-18 12:08:15 +0200

    Hangman.cbp, Hangman.depend und 9 weitere dateien aktualisiert...

---
+++ Hangman/input_header.h
@@ -0,0 +1,28 @@
+#ifndef INPUT_HEADER_H_INCLUDED
+#define INPUT_HEADER_H_INCLUDED
+
+/*
+ * Outputs the Game Start Dialog
+ */
+void printGameStart();
+
+/*
+ * Validates if input is alpha and only one char
+ * Returns 1 on success
+ * Returns 0 in fail, prints out respective error message
+ */
+int inputValidation(char input[]);
+
+/*
+ * Requires Fails as parameter
+ * Prints out current Hangman Ascii
+ * prints out fails and tries left
+ */
+void printStatus(int i);
+
+/*
+ * Clears the console screen
+ */
+void clearScreen();
+
+#endif // INPUT_HEADER_H_INCLUDED
