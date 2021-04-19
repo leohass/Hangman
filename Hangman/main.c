372b97e0a5b7c05d238e81bc1ecb2a90b67fc2fb
ifa02.timmjuli <1julian.timmer1@gmail.com>
2021-04-19 09:14:35 +0200

    alphabetMask

--- Hangman/main.c
+++ Hangman/main.c
@@ -1,24 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
-const maxMistakes = 10;
+const int maxMistakes = 7;
+const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-char player1[];
+char player1[] = "";
 //char player2[];
-char solution[];
-char hangmanPrint[];
+char solution[] = "JULIAN"; //set solution for testing
+char hangmanPrint[] = "";
 
 int start; //GetTickCount(int)
 
 int tries;
-char triedLetters[];
-char correctLetters[];
+char triedLetters[26] = "";
+char correctLetters[] = "";
+
+int alphabetMask[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int mistakes;
 int success = 0;
 
+
+void reset_game()
+{
+    start = 0;
+    tries = 0;
+    mistakes = 0;
+    success = 0;
+
+    for(int u = 0; u < strlen(alphabet); u++)
+        {
+            alphabetMask[u] = 0;
+        }
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    reset_game();
+
+    int length = strlen(solution);
+    int solutionMask[length];
+    for (int i = 0; i < length; i++) {
+        solutionMask[i] = 0;
+    }
+
+    while (mistakes <= maxMistakes && success == 0)
+    {
+        //hier input einbauen
+        char validInput;
+        printf("Mistakes: %i\n", mistakes);
+        fflush(stdout);
+        printf("Guess: ");
+        fflush(stdout);
+        scanf(" %c", &validInput);
+
+        tries++;
+
+        for(int j = 0; j < 26; j++)
+        {
+            if (alphabet[j] == validInput)
+            {
+                alphabetMask[j] = 1;
+            }
+        }
+
+        for(int m = 0; m < strlen(alphabet); m++)
+        {
+            if (alphabetMask[m] == 1)
+            {
+                printf("%c", alphabet[m]);
+                printf(" ");
+            }
+        }
+        printf("\n");
+
+        int mistakeMade = 1;
+
+        for(int k = 0; k < length; k++)
+        {
+            if (solution[k] == validInput)
+            {
+                solutionMask[k] = 1;
+                mistakeMade = 0;
+            }
+        }
+
+        if (mistakeMade == 1)
+        {
+            mistakes++;
+        }
+
+        success = 1;
+        for(int l = 0; l < length; l++)
+        {
+            if (solutionMask[l] == 0)
+            {
+                success = 0;
+            }
+        }
+
+    }
+
+    if (success == 1) {
+        printf("\nWin!");
+    }
+    else
+    {
+        printf("\nLose :(");
+    }
+
+
     return 0;
 }
